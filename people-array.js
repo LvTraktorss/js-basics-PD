84baa4b66bf4f3f0056e9c8b5379d8969472a8ea
Your Name <you@example.com>
2025-10-27 11:22:54 +0200

    pievienoju people-array

---
+++ people-array.js
@@ -0,0 +1,8 @@
+let people =[
+{name:"Paula", age:"15", isStudent:true},
+{name:"Emīls", age:"24", isStudent:false}
+]
+for (let i = 0; i < people.length; i ++){
+    people= people.length[i]
+    console.log("Vārds")
+}
