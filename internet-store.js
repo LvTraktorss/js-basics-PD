550a9f6e2db95c229e371db01d77c795ba29acb4
Your Name <you@example.com>
2025-10-27 11:39:57 +0200

    pievienoju internet-store

---
+++ internet-store.js
@@ -0,0 +1,7 @@
+let inventory=[
+{name:"Jaka", category:"Drēbes", price:40,inStorage: 15},
+{name:"Monitors", category:"Elektronika", price:120, inStorage: 23},
+{name:"Proccesors", category:"Elektronika", price:300,inStorage: 5},
+{name:"Bikses", category:"Dēbes", price:45, inStorage: 25}
+]
+
